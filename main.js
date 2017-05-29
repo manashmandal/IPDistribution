cada9eb94099b5260afa448388e61b169110cab6
Manash Kumar Mandal <manashmndl@gmail.com>
2017-05-29 17:09:00 +0600

    add front end file

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var ping = require('ping');
 var gen_ip = require('./helpers/ip').generate_ip_list;
 var fs = require('fs');
+var express = require('express');
+var app = express();
+const path = require("path");
 
 const root = __dirname;
 
@@ -8,6 +11,10 @@ const root = __dirname;
 const read_ip_path = root + '\\files\\bd_ip_list.csv';
 const write_ip_path = root + '\\files\\all_ip.csv';
 
+// Set view enine 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, "views"));
+app.use(express.static('public'))
 
 // Do this only once!
 if (fs.existsSync(write_ip_path)) {
@@ -16,3 +23,15 @@ if (fs.existsSync(write_ip_path)) {
     console.log("FILE DOESN'T EXIST, WRITING....");
     gen_ip(read_ip_path, write_ip_path);
 }
+
+app.get('/:user', function (req, res) {
+    res.render("homepage", {
+        user: req.params.user
+    });
+});
+
+app.get('/', function (req, res) {
+    res.render("homepage");
+})
+
+app.listen(8000);
