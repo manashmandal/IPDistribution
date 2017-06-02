88ea8aa6317d3c6b587c34cb79eabc66891f3a1a
Manash Kumar Mandal <manashmndl@gmail.com>
2017-06-03 03:51:11 +0600

    add module

--- models/IPModel.js
+++ models/IPModel.js
@@ -1,12 +1,53 @@
 var mongoose = require('mongoose');
+const doc_name = 'ip_db';
 
 var IPSchema = new mongoose.Schema({
     id: {
-        type: String,
-        index: true
+        type: String
     },
 
-    data: {
+    data: [{
+        host: {
+            type: String
+        },
+        timestamp: {
+            type: Date
+        },
+        alive: {
+            type: Boolean
+        },
+        output: {
+            type: String
+        },
 
-    }
+        time: {
+            type: Number
+        },
+
+        min: {
+            type: Number
+        },
+
+        max: {
+            type: Number
+        },
+
+        avg: {
+            type: Number
+        },
+
+        stddev: {
+            type: Number
+        },
+
+        numeric_host: {
+            type: String
+        }
+    }]
 });
+
+var IP = mongoose.model('IP', IPSchema, doc_name);
+
+module.exports = {
+    IP: IP
+}
